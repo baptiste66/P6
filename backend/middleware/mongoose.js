9a6f0944f47c94a7586dbfa265b3a4108b13789d
baptiste66 <101480985+baptiste66@users.noreply.github.com>
2023-02-24 15:50:20 +0100

    condition mot de passe

--- backend/middleware/mongoose.js
+++ backend/middleware/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const user=process.env.mongoose_user
 const password= process.env.mongoose_password
 //connexion mongoose
-mongoose.connect(`mongodb+srv://${user}:${password}@cluster2.thsotrz.mongodb.net/?retryWrites=true&w=majority`,
+mongoose.connect(`mongodb+srv://${user}:${password}@cluster2.thsotrz.mongodb.net/test`,
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
--- backend/models/password.js
+++ backend/models/password.js
@@ -2,11 +2,15 @@ var passwordValidator = require('password-validator');
 var passwordSchema = new passwordValidator();
 // le modèle du mot de passe
 passwordSchema
+//8 a 32 lettre
 .is().min(8)                                   
-.is().max(32)                                  
+.is().max(32)        
+//maj min                          
 .has().uppercase(1)                           
-.has().lowercase(1)                              
+.has().lowercase(1)    
+// symbole spécial                          
 .has().symbols(1)
+// nombre
 .has().digits(1)                              
 .has().not().spaces()
 module.exports=passwordSchema                           
